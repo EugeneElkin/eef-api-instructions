d6340318c19557a3d8c4a912c9a5797360b7a805
Evgenii Elkin <v-evelk@microsoft.com>
2019-06-18 14:08:26 +0300

    Class renaming: BaseEntityWithParentUserContexted to BaseEntityWithParentAndUserContext

---
+++ BaseEntities/Entities/BaseEntityWithParentAndUserContext.cs
@@ -0,0 +1,10 @@
+﻿namespace EEFApps.ApiInstructions.BaseEntities.Entities
+{
+    using EEFApps.ApiInstructions.BaseEntities.Entities.Interfaces;
+
+    public abstract class BaseEntityWithParentAndUserContext<TId, TUserId> : BaseEntity<TId>, IEntityWithUserContext<TUserId>, IEntityWithParent<TId>
+    {
+        public TUserId UserId { get; set; }
+        public TId ParentId { get; set; }
+    }
+}
